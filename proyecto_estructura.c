7cdcc3349915f5ea18248338d548c32bff0d0135
Marti <martina.sandoval.o@mail.pucv.cl>
2024-06-07 19:09:08 -0400

    struct

--- proyecto_estructura.c
+++ proyecto_estructura.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int data;
+} tipoDato;
 
 int main(void)
 {
+    tipoDato dato;
+    dato.data = 13;
     printf("hola\n");
     int a = 1;
     int b = 4;
     printf("%d", a + b);
     printf("odio esto\n");
+
+    printf(dato.data);
     return 0;
 }

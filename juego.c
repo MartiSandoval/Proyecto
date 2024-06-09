c55a4bd61bd95f4e3f21f6b004063b2c6b727eda
Marti <martina.sandoval.o@mail.pucv.cl>
2024-06-09 14:55:00 -0400

    arreglando algunas cosas básicas

--- juego.c
+++ juego.c
@@ -3,37 +3,49 @@
 //#include <stdbool.h>
 //#include "tdas/list.h"
 //#include <string.h>
+
+void comoJugar() {
+    printf("\nprobando que funcione bien todo...\n");
+}
 int main(void)
 {
     char opcion;
     do
     {
-        puts("NOMBRE DEL JUEGO"); // hay que decidir un nombre del juego...
+        printf("NOMBRE DEL JUEGO\n\n"); // hay que decidir un nombre del juego...
 
-        puts("(1) Jugar");
-        puts("(2) Cargar Partida");
-        puts("(3) ¿Como Jugar?");
-        puts("(4) Salir");
+        printf("(1) Jugar\n");
+        printf("(2) Cargar Partida\n");
+        printf("(3) ¿Como Jugar?\n");
+        printf("(4) Salir\n");
 
         printf("Ingrese su opcion: ");
-        scanf_s("%c", opcion);
-        printf("%c", opcion);
+        scanf(" %c", &opcion);
+        printf("\n");
+
         switch (opcion)
         {
             case '1':
+                printf("elegiste la 1\n\n");
+                //jugar();
                 break;
             case '2':
+                printf("elegiste 2\n\n");
+                //cargarPartida();
                 break;
             case '3':
+                comoJugar();
+                printf("\n");
                 break;
             case '4':
-                puts("Salendo del juego...");
+                printf("Saliendo del juego...\n");
                 break;
             default:
-                puts("Por favor ingresar una opcion valida");
+                printf("Por favor ingresar una opcion valida\n");
                 break;
         }
 
     } while (opcion != '4');
+
     return 0;
 }
